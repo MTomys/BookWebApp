7f49744b1002512fc52e78764e0e5846f4b6cf49
MTomys <michalltomys@protonmail.ch>
2022-06-03 10:06:08 +0200

    Add models

---
+++ BookWebApp/BookWebApp/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookWebApp.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string IsbnNumber { get; set; }
+        public string BookName { get; set; }
+        public string BookGenre { get; set; }
+        public int PageCount { get; set; }
+        public int BookAuthorId { get; set; }
+    }
+}
---
+++ BookWebApp/BookWebApp/Models/BookAuthor.cs
@@ -0,0 +1,10 @@
+﻿namespace BookWebApp.Models
+{
+    public class BookAuthor
+    {
+        public int BookAuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nationality { get; set; }
+    }
+}
