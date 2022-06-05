6f5f13fb47d1955242fb9e87b040b91e26e5a879
MTomys <michalltomys@protonmail.ch>
2022-06-05 10:54:59 +0200

    Modify model attributes

--- BookWebApp/BookWebApp/Models/Book.cs
+++ BookWebApp/BookWebApp/Models/Book.cs
@@ -3,10 +3,13 @@
     public class Book
     {
         public int BookId { get; set; }
+
         public string IsbnNumber { get; set; }
         public string BookName { get; set; }
         public string BookGenre { get; set; }
         public int PageCount { get; set; }
+
         public int BookAuthorId { get; set; }
+        public virtual Book BookAuthor { get; set; }
     }
 }
--- BookWebApp/BookWebApp/Models/BookAuthor.cs
+++ BookWebApp/BookWebApp/Models/BookAuthor.cs
@@ -1,10 +1,13 @@
 ﻿namespace BookWebApp.Models
 {
-    public class BookAuthor
+    public class Book
     {
         public int BookAuthorId { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Nationality { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
     }
 }
