fba11cfed02dbd258bf54ca1901a7f3bc3a4697e
MTomys <michalltomys@protonmail.ch>
2022-07-19 22:31:08 +0200

    Refactor - remove BookItem to allow single selection functionality

--- BookWebApp/BookWebAppUI/src/components/BookTable.tsx
+++ BookWebApp/BookWebAppUI/src/components/BookTable.tsx
@@ -1,13 +1,19 @@
 import { mockBooks } from '../mock-data/index';
 import { bookColumns } from '../mock-data/books/bookColumns';
-import { useRowSelect, useTable } from 'react-table';
-import { BookTableItem } from './BookTableItem';
+import { useState } from 'react';
+import { useRowSelect, useTable, Row } from 'react-table';
+import { EditBookButton } from './EditBookButton';
+import { RemoveBookButton } from './RemoveBookButton';
+import { Book } from '../interfaces/index';
 
 export const BookTable = () => {
   const columns = bookColumns;
   const data = mockBooks;
 
-  const tableInstance = useTable({ columns, data }, useRowSelect);
+  const [selectedRow, setSelectedRow] = useState<Row<Book>>();
+  const [isItemSelected, setIsItemSelected] = useState(false);
+
+  const tableInstance = useTable({ columns, data });
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     tableInstance;
@@ -28,7 +34,48 @@ export const BookTable = () => {
         <tbody {...getTableBodyProps()}>
           {rows.map((row) => {
             prepareRow(row);
-            return <BookTableItem row={row} />;
+            return (
+              <tr
+                className={`group border text-center select-auto hover:bg-slate-200 transition cursor-pointer ${
+                  isItemSelected &&
+                  `shadow-sm border-1 border-slate-400 bg-slate-300 hover:bg-slate-300`
+                }`}
+                {...row.getRowProps()}
+              >
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      onClick={() => {
+                        setIsItemSelected(!isItemSelected);
+                      }}
+                      {...cell.getCellProps()}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+                <span className="cursor-default">
+                  <td
+                    className={`group-hover:visible ${
+                      isItemSelected ? `visible` : `invisible`
+                    } `}
+                  >
+                    <EditBookButton
+                      isParentSelected={isItemSelected}
+                    ></EditBookButton>
+                  </td>
+                  <td
+                    className={`group-hover:visible ${
+                      isItemSelected ? `visible` : `invisible`
+                    } `}
+                  >
+                    <RemoveBookButton
+                      isParentSelected={isItemSelected}
+                    ></RemoveBookButton>
+                  </td>
+                </span>
+              </tr>
+            );
           })}
         </tbody>
       </table>
