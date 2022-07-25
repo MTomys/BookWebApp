675c144db04d0360c005d64930e1618819fa5dd1
MTomys <michalltomys@protonmail.ch>
2022-07-25 14:26:16 +0200

    Add global usings, add ServiceResult helper class

---
+++ BookWebApp/BookWebAppCore/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System;
+global using BookWebAppCore.Models;
+global using BookWebAppCore.Interfaces;
+global using BookWebAppCore.Data;
+global using BookWebAppCore.Dtos;
---
+++ BookWebApp/BookWebAppCore/Utils/ServiceResult.cs
@@ -0,0 +1,9 @@
+﻿namespace BookWebAppCore.Utils
+{
+    public class ServiceResult<T>
+    {
+        public T Data { get; init; }
+        public bool IsOperationSuccessful { get; init; }
+        public string OperationMessage { get; init; } 
+    }
+}
